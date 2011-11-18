28ae1b7addd1876f02fd649b3998d22eafb042e0
Andrey Morozov <andrew.morozov@gmail.com>
2011-11-18 20:27:11 +0100

    almost

---
+++ src/epa.py
@@ -0,0 +1,203 @@
+'''
+This module contains a singleton class with several methods for computation of the error propagation graph
+Created on Jun 15, 2011
+@author: andrey
+'''
+
+import numpy as np
+
+class analyzer(object):
+    """EPG analyzer"""
+    def __init__(self):
+        #Singletone stuff
+        _instance = None
+        def __new__(cls, *args, **kwargs):
+            if not cls._instance:
+                cls._instance = super(analyzer, cls).__new__(
+                                    cls, *args, **kwargs)
+            return cls._instanc
+
+
+    def direct_compute(self,G):
+        """The method for direct computation of the given EPG. Based on numpy's linear solver"""
+        l=len(G.S)
+        P=np.zeros([l,l])
+        pointers = []
+        print "direct_compute started"
+        print "Matrix creation, size:" + str(l)+'x'+str(l)
+        for s in G.S:
+            pointers.append(s)
+        for a in G.A:
+            for i in range(l):
+                if (pointers[i] == a.s1):
+                    break
+            for j in range(l):
+                if (pointers[j] == a.s2):
+                    break
+            P[i,j]=a.pr
+        print "done"
+        print "Computation"
+        I=np.identity(l)
+        N=np.linalg.solve(I-P,I)
+        print "done"
+        for i in range(l):
+            if pointers[i] == G.initial:
+                break
+        for j in range(l):
+            pointers[j].pr = N[i,j]
+        print "direct_compute complete"    
+    
+    
+    
+    def iter_compute(self, G, accuracy):
+        """A simple method for iterative computation of the given EPG.
+        An accuracy parameter controls method execution duration
+        """
+        print 'iter_compute started'
+        print 'given accuracy: '+str(accuracy)
+        
+        for s in G.S:
+            s.pr = 0
+            s.next_pr = 0
+        G.initial.pr = 1
+        cur_acc = 1
+        
+        iter = 0
+        while cur_acc > accuracy:
+            iter = iter+1
+            cur_acc = 1
+            for s in G.S:
+                if s.pr>0:
+                    if len(s.O)>0:
+                        #transient
+                        for a in s.O:
+                            a.s2.next_pr = a.s2.next_pr + s.pr * a.pr
+                    else:
+                        #absorbing
+                        s.next_pr = s.next_pr + s.pr
+                        cur_acc = cur_acc - s.pr
+                    
+            for s in G.S:
+                s.pr = s.next_pr
+            for s in G.S:
+                s.next_pr = 0.0
+
+        print 'iter_compute complete'
+        print 'iteration number: '+str(iter)
+        print 'achieved accuracy: '+str(cur_acc)
+    
+ 
+    
+    def loop_reduce(self,G,s_num,prints=False):
+        for loop_arc in G.LA:
+            if len(G.S) > s_num:                   
+                for ao in loop_arc.s1.O:
+                    if ao!=loop_arc:
+                        ao.pr=ao.pr/(1-loop_arc.pr)
+                if prints:           
+                    print loop_arc.get_name() + ' - deleted'
+                G.remove_arc(loop_arc)  
+                return True
+        return False                       
+    
+    
+    
+    def state_reduce(self,G,s_num,initial,prints=False):
+        for b in G.S:
+            if len(G.S) > s_num:
+                #check
+                #only one input
+                if len(b.I)!=1:
+                    continue
+                #not an absorption one
+                if len(b.O)==0:
+                    continue
+                #not initial
+                if b == initial:
+                    continue
+                #no self loops
+                self_arc = False
+                for i in b.I:
+                    if i.s1 == i.s2:
+                        self_arc = True
+                        break
+                if self_arc:
+                    continue
+    
+                #TODO check semi absorptions
+                #go
+    
+                a_cb = b.I[0]
+                for a_bd in b.O:
+                    a_cd = G.get_arc(a_cb.s1,a_bd.s2)
+                    if a_cd == None:
+                        a_cd=G.add_arc(a_cb.s1,a_bd.s2,0.0)
+                    a_cd.pr = a_cb.pr*a_bd.pr+a_cd.pr
+                if prints:               
+                    print b.name + ' - deleted'       
+                G.remove_state(b)
+                return True
+        return False
+    
+    def arc_reduce(self,G,s_num,initial,prints=False):
+        profit = False
+        for b in G.S:
+            if len(b.O) == 0:
+                continue
+            if len(b.I) <= 1:
+                continue        
+            for a_ab in b.I:
+                if len(G.S) > s_num:
+                    if a_ab.s1 == initial:
+                        continue
+                    if a_ab.s1 == a_ab.s2:
+                        continue
+                    self_arc = False
+                    for i in a_ab.s2.I:
+                        if i.s1 == i.s2:
+                            self_arc = True
+                            break
+                    if self_arc:
+                        continue
+                    
+                    for a_bc in a_ab.s2.O:
+                        if a_bc.s2 != a_ab.s2:
+                            a_ac = G.get_arc(a_ab.s1,a_bc.s2)
+                            if a_ac == None:
+                                a_ac=G.add_arc(a_ab.s1,a_bc.s2,0.0)
+                            a_ac.pr = a_ab.pr*a_bc.pr+a_ac.pr
+                    if prints:
+                        print a_ab.get_name() + ' - deleted'        
+                    G.remove_arc(a_ab)
+                    profit = True
+            if profit:
+                return True
+        return False
+    
+    
+            
+    def reduce(self,G,s_num,prints=False):
+        """The method for EPG reduction using the loop reduction, state reduction, and arc reduction.
+        s_num - is a number of states that limits the reduction process"""
+        initial = G.initial
+        print 'reduction started'
+        print 'original state number:'+str(len(G.S))
+        G.get_absorbing_S_num()
+        while len(G.S) > s_num:
+            p1 = self.state_reduce(G,s_num,initial,prints)
+            if not p1:
+                p2 = self.loop_reduce(G,s_num,prints)
+                if not p2:
+                    p3 = self.arc_reduce(G,s_num,initial,prints)
+                    if not p3:
+                        break
+            else:
+                to_del =len(G.S)-G.absorbing_num
+                if (to_del % 100 == 0):
+                    print 'States to be reduced < ' + str(to_del)              
+        print 'reduction complete'
+        print 'reduced state number:'+str(len(G.S))
+        return G
+
+
+        
---
+++ src/viz.py
@@ -0,0 +1,135 @@
+'''
+Visualization of the error propagation model
+
+Created on Jun 5, 2011
+@author: andrey
+'''
+
+import os
+import pygraphviz as pgv
+import epm
+
+class viz(object):
+    """Class for EPF visualization"""
+    
+    def make_e_name(self, e, no_el_data = False):
+        """
+        Create an output name for an element of an EPM,
+        no_el_data = True - name only
+        no_el_data = False - name plus element properties: FAP, EPP, EDP, EDB
+        """
+        if no_el_data:
+            return e.name
+        else:
+            s = e.name + '\\n' + 'fap: ' + str(e.fap) + '  epp: ' + str(e.epp) + '\\n' + 'edp: ' + str(e.edp) + '  edb: ' + str(e.edb)
+            return s
+        
+    def draw_epm(self, S, no_el_data = False, no_pr = False):
+        """
+        Draw CFG and DFG of the EPM as .pdf files to the folder with the name of the EPM
+        no_el_data - do not show element properties
+        no_pr - do not show transition probabilities of the CFG
+        """
+        try:
+            os.mkdir(S.name)
+        except:
+            pass
+        CFG = pgv.AGraph(directed=True)
+        CFG.add_node(self.make_e_name(S.initial,no_el_data),color="blue")
+        for a in S.ACF:
+            if no_pr:
+                CFG.add_edge(self.make_e_name(a.e1,no_el_data), self.make_e_name(a.e2,no_el_data))
+            else:
+                CFG.add_edge(self.make_e_name(a.e1,no_el_data), self.make_e_name(a.e2,no_el_data), label=str(a.pr))
+        
+        DFG = pgv.AGraph(directed=True)
+        DFG.add_node(self.make_e_name(S.initial,no_el_data),color='blue')
+        for a in S.ADF:
+            DFG.add_edge(self.make_e_name(a.e1,no_el_data), self.make_e_name(a.e2,no_el_data))
+            
+        CFG.edge_attr.update(color='blue')
+        DFG.edge_attr.update(color='darkgreen')            
+        CFG.draw(S.name+'/CFG.pdf',prog='dot')
+        DFG.draw(S.name+'/DFG.pdf',prog='dot')
+           
+    
+    def make_s_name(self, s, noprob = False):
+        """Creates an output name for a given state"""
+        NXT = ''
+        if isinstance(s.e_next, str):
+            NXT = s.e_next
+        else:
+            NXT = str(s.e_next.name)
+        st = s.name + '\\n' + 'nxt:' + NXT
+        EFA = ''
+        EEP = ''
+        EED = ''
+        for e in s.EFA:
+            EFA = EFA + e.name + ' '
+        if EFA!='':
+            st = st + '\\n' + 'FA:' + EFA
+        for e in s.EEP:
+            EEP = EEP + e.name + ' '
+        if EEP!='':
+            st = st + '\\n' + 'EP:' + EEP
+        for e in s.EED:
+            EED = EED + e.name + ' '
+        if EED!='':
+            st = st + '\\n' + 'ED:' + EED
+        
+        if not noprob:
+            if len(s.O) ==0:
+                st = st + '\\n' + 'Pr:' + str(s.pr)[0:7]
+        
+        return st      
+    
+    def draw_epg(self, G, name = ''):
+        """Draw EPG to a pdf file using the graphviz library"""
+        try:
+            os.mkdir(G.EPM.name)
+        except:
+            pass
+        EPG = pgv.AGraph(directed=True)
+        EPG.node_attr['style']='filled'
+        for s in G.S:
+            if len(s.O) == 0:
+                if len(s.EEP) == 0:
+                    EPG.add_node(self.make_s_name(s))
+                    n = EPG.get_node(self.make_s_name(s))
+                    n.attr['fillcolor']="blue"
+                else:
+                    if len(s.EED) > 0:
+                        EPG.add_node(self.make_s_name(s))
+                        n = EPG.get_node(self.make_s_name(s))
+                        n.attr['fillcolor']="purple"
+                    else:
+                        EPG.add_node(self.make_s_name(s))
+                        n = EPG.get_node(self.make_s_name(s))
+                        n.attr['fillcolor']="red"
+            else:
+                if len(s.EEP) > 0:
+                    EPG.add_node(self.make_s_name(s),color="red")
+                    n = EPG.get_node(self.make_s_name(s))
+                    n.attr['fillcolor']="white"
+                else:
+                    EPG.add_node(self.make_s_name(s),color="blue")
+                    n = EPG.get_node(self.make_s_name(s))
+                    n.attr['fillcolor']="white"           
+        for a in G.A:
+            EPG.add_edge(self.make_s_name(a.s1), self.make_s_name(a.s2), label=str(a.pr)[0:7])
+        
+        if name == '':
+            EPG.draw(G.EPM.name+'/EPG.pdf',prog='dot')
+        else:
+            EPG.draw(G.EPM.name+'/'+name+'.pdf',prog='dot')
+
+
+
+
+        
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
