21b352ab7ca3c7c908cc77f12514a55dd5ee357d
Andrey Morozov <andrew.morozov@gmail.com>
2011-11-18 20:30:14 +0100

    almost3

--- src/sandbox.py
+++ src/sandbox.py
@@ -4,7 +4,7 @@ Created on Nov 16, 2011
 @author: andrey
 '''
 
-import epf_epm as epm
+import epm
 
 S = epm.system("ABC System")
 S.add_element('A', 0.1, 1, 0.0)
@@ -17,21 +17,21 @@ S.add_cf_arc('B', 'C', 0.1)
 S.add_df_arc('A', 'B')
 S.add_df_arc('B', 'C')
 
-import epf_viz as viz
+import viz
 
 V = viz.viz()
 V.draw_epm(S)
 
-import epf_epg as epg
+import epg
 G = epg.graph()
 G.generate(S)
 
-import epf_epa as epa
+import epa
 A = epa.analyzer()
-
-A.iter_compute(G,0.000000000001)
+A.iter_compute(G,0.001)
+G.print_results(100)
 A.direct_compute(G)
-G.show_results(10)
+G.print_results(100)
 V.draw_epg(G)
 
 
